8531ee1d092b3e5f31ea0979c75419791e7c1f17
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 15:02:59 +0700

    Create It's getting cold outside, W20529.py

---
+++ 4.Easy and Powerful: Dates and Times in Pandas/It's getting cold outside, W20529.py
@@ -0,0 +1,25 @@
+"""Resample rides to the daily level, based on the Start date column.
+Plot the .size() of each result."""
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Resample rides to daily, take the size, plot the results
+rides.resample('D', on = 'Start date')\
+  .size()\
+  .plot(ylim = [0, 15])
+
+# Show the results
+plt.show()
+
+
+"""Since the daily time series is so noisy for this one bike, change the resampling to be monthly."""
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Resample rides to monthly, take the size, plot the results
+rides.resample('M', on = 'Start date')\
+  .size()\
+  .plot(ylim = [0, 150])
+
+# Show the results
+plt.show()
