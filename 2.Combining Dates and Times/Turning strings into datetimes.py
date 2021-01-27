3956e06b0bf3c2125ff88282e986d9e21d4d9ce4
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:47:35 +0700

    Update Turning strings into datetimes.py

--- 2.Combining Dates and Times/Turning strings into datetimes.py
+++ 2.Combining Dates and Times/Turning strings into datetimes.py
@@ -32,3 +32,21 @@ d = datetime.strptime(s, fmt)
 
 # Print d
 print(d)
+
+
+"""Determine the format needed to convert s to datetime and assign it to fmt.
+Convert the string s to datetime using fmt."""
+# Import the datetime class
+from datetime import datetime
+
+# Starting string, in MM/DD/YYYY HH:MM:SS format
+s = '12/15/1986 08:00:00'
+
+# Write a format string to parse s
+fmt = '%m/%d/%Y %H:%M:%S'
+
+# Create a datetime object d
+d = datetime.strptime(s, fmt)
+
+# Print d
+print(d)
