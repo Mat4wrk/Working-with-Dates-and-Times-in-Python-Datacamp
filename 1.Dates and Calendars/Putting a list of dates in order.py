85e2ed1b9ff970ea81dd10f30a083e49754038f7
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:41:09 +0700

    Create Putting a list of dates in order.py

---
+++ 1.Dates and Calendars/Putting a list of dates in order.py
@@ -0,0 +1,18 @@
+"""Print the first and last dates in dates_scrambled.""
+# Print the first and last scrambled dates
+print(dates_scrambled[0])
+print(dates_scrambled[-1])
+
+
+"""Sort dates_scrambled using Python's built-in sorted() method, and save the results to dates_ordered."""
+"""Print the first and last dates in dates_ordered."""
+# Print the first and last scrambled dates
+print(dates_scrambled[0])
+print(dates_scrambled[-1])
+
+# Put the dates in order
+dates_ordered = sorted(dates_scrambled)
+
+# Print the first and last ordered dates
+print(dates_ordered[0])
+print(dates_ordered[-1])
