d6d89fc031982ff669048c7d8f325d7dfc1d81c7
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-27 14:38:40 +0700

    Create Which day of the week?.py

---
+++ 1.Dates and Calendars/Which day of the week?.py
@@ -0,0 +1,22 @@
+"""Import date from datetime."""
+# Import date from datetime
+from datetime import date
+
+
+"""Create a date object for August 24, 1992."""
+# Import date from datetime
+from datetime import date
+
+# Create a date object
+hurricane_andrew = date(1992, 8, 24)
+
+
+"""Now ask Python what day of the week Hurricane Andrew hit (remember that Python counts days of the week starting from Monday as 0, Tuesday as 1, and so on)."""
+# Import date from datetime
+from datetime import date
+
+# Create a date object
+hurricane_andrew = date(1992, 8, 24)
+
+# Which day of the week is the date?
+print(hurricane_andrew.weekday())
